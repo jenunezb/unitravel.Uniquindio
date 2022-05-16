1421569803beb041b1a022ad29710430e850478b
proteuz <jenunezb@uqvirtual.edu.co>
2022-05-16 06:11:47 -0500

    Entidades de la biblioteca y gitignore

--- persistencia/src/main/java/co/edu/uniquindio/unitravel/PersistenciaApplication.java
+++ persistencia/src/main/java/co/edu/uniquindio/unitravel/PersistenciaApplication.java
@@ -1,7 +1,7 @@
 package co.edu.uniquindio.unitravel;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class PersistenciaApplication {
---
+++ persistencia/src/main/java/co/edu/uniquindio/unitravel/entidades/Genero.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.unitravel.entidades;
+
+public enum Genero {
+
+    MISTERIO,
+    TERROR,
+    ROMANCE,
+    FANTASIA,
+    HISTORIA,
+    INGENIERIA,
+    NOVELA
+
+}
