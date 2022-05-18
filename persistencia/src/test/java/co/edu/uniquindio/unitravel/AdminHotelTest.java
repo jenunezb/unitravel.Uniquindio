38a25d98be1c2c904f9addbd7c53a64f69ec6b47
proteuz <jenunezb@uqvirtual.edu.co>
2022-05-18 09:12:48 -0500

    Test Administrador completo

--- persistencia/src/test/java/co/edu/uniquindio/unitravel/AdminHotelTest.java
+++
@@ -1,4 +0,0 @@
-package co.edu.uniquindio.unitravel;
-
-public class AdminHotelTest {
-}
--- persistencia/src/test/resources/dataset.sql
+++
@@ -1,7 +0,0 @@
-insert into ciudad values (2, "Pereira");
-insert into ciudad values (3, "Manizales");
-insert into ciudad values (4, "Bogota");
-
-insert into usuario values ("1", "lorena@email.com", "Lorena Lopez", "123",1);
-insert into usuario values ("2", "julian@email.com", "Julian", "123",2);
-insert into usuario values ("3", "esteban@email.com", "Esteban", "123",3);
