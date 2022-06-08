8dd9529ac2a5f2936ecb3b30332ad357f9469a86
proteuz <jenunezb@uqvirtual.edu.co>
2022-06-08 13:25:10 -0500

    Bootstrap y organización

--- negocio/src/main/java/co/edu/uniquindio/unitravel/repositorios/AdminHotelRepo.java
+++
@@ -1,9 +0,0 @@
-package co.edu.uniquindio.unitravel.repositorios;
-
-import co.edu.uniquindio.unitravel.entidades.AdminHotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdminHotelRepo extends JpaRepository<AdminHotel, String>{
-}
